ce9ad2b207c1a728ceb2ca1956b5cac61fee2f56
Ldsc2002 <luissantoscuellar@gmail.com>
2023-10-15 19:23:27 -0600

    Changes to model and app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 from keras.preprocessing import image
 import numpy as np
+import json
 import matplotlib.pyplot as plt
 from keras.models import load_model
-from modelo import predict_mosquito_type
 
 app = Flask(__name__)
 
@@ -12,6 +12,7 @@ UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 model = load_model('best_model.h5')
+classIndex = json.load(open("class_indices.json"))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -34,6 +35,18 @@ def index():
             return f"Predicción: {prediction}"
     return render_template('index.html')  
 
+def predict_mosquito_type(model, img_path):
+    img = image.load_img(img_path, target_size=(150, 150))
+    img_array = image.img_to_array(img) / 255.
+    img_batch = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_batch)
+    predicted_index = np.argmax(prediction)
+
+    labels = dict((v, k) for k, v in classIndex.items())  # flip the key, values in the dictionary
+    predicted_label = labels[predicted_index]
+
+    return predicted_label
 
 if __name__ == '__main__':
     app.run(debug=True)
